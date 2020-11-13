23649d19775105ba0e3fcedf2e372e2c079e8a3f
moinz <55895626+MAG199@users.noreply.github.com>
2020-11-12 18:04:57 -0600

    Update Solution.java

--- Search in rotated sorted array/Solution.java
+++ Search in rotated sorted array/Solution.java
@@ -1,3 +1,7 @@
+//Time Complexity: O(logn)
+//Space Complexity: O(1)
+//Did this code successfully run on Leetcode: Yes
+
 class Solution {
     public int search(int[] nums, int target) {
         // for(int i=0;i<nums.length;i++){
